97cf13ac37a7dd065111f66b774cb98ac3819cc4
Proskurina Arina <36183877+arinaproskurina797@users.noreply.github.com>
2020-12-26 11:03:13 +0700

    Initial commit

---
+++ app/src/main/cpp/opengl.h
@@ -0,0 +1,12 @@
+//
+// Created by User on 029 29.11.18.
+//
+
+#ifndef LAB5_NOTOKNORM_OPENGL_H
+#define LAB5_NOTOKNORM_OPENGL_H
+
+void on_surface_created();
+void on_surface_changed();
+void on_draw_frame();
+
+#endif //LAB5_NOTOKNORM_OPENGL_H
